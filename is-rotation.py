dda5c4063b738182f3254c213aae4b8a5232be59
Amy Hashemi <amyhashemi@gmail.com>
2019-07-20 18:39:40 -0700

    modified is-rotation, and solved one-away

--- is-rotation.py
+++ is-rotation.py
@@ -12,11 +12,11 @@ list2a = [4, 5, 6, 7, 8, 1, 2, 3]
 list2b = [4, 5, 6, 7, 1, 2, 3]
 # is_rotation(list1, list2b) should return True.
 list2c = [4, 5, 6, 9, 1, 2, 3] #######
-# is_rotation(list1, list2c) should return False. - this
+# is_rotation(list1, list2c) should return False.
 list2d = [4, 6, 5, 7, 1, 2, 3] #######
-# is_rotation(list1, list2d) should return False. - this
+# is_rotation(list1, list2d) should return False.
 list2e = [4, 5, 6, 7, 0, 2, 3] #######
-# is_rotation(list1, list2e) should return False. - this
+# is_rotation(list1, list2e) should return False.
 list2f = [1, 2, 3, 4, 5, 6, 7]
 # is_rotation(list1, list2f) should return True.
 list2g = [7, 1, 2, 3, 4, 5, 6]
--- one-away.py
+++ one-away.py
@@ -1,8 +1,60 @@
-def one_away(string1, string2):
+def is_one_away(string1, string2):
     '''
     Returns True if either string1 or string2 are within one 
     edit away of being identical to one another. Operations include:
     Change: Changing a single character
-    Delete: Deleting a singla character
+    Delete: Deleting a single character
     Add: Adding a single character
     '''
+
+    p1 = 0
+    p2 = 0
+
+    editcount = 0
+
+    if abs(len(string1) - len(string2)) > 1:
+        return False
+    
+    if len(string1) == len(string2):
+        while p1 < len(string1) and p2 < len(string2):
+            if string1[p1] != string2[p2]:
+                editcount+=1
+            p1+=1
+            p2+=1
+        if editcount > 1:
+            return False
+    
+    if abs(len(string1) - len(string2)) == 1:
+        while p1 < len(string1) and p2 < len(string2):
+            if string1[p1] != string2[p2]:
+                if len(string1) > len(string2):
+                    p1+=1
+                if len(string1) < len(string2):
+                    p2+=1
+                if string1[p1] != string2[p2]:
+                    return False
+            p1+=1
+            p2+=1
+    
+    return True
+
+ans = is_one_away("abcde", "abcd")  # should return True
+print(ans)
+ans = is_one_away("abde", "abcde")  # should return True
+print(ans)
+ans = is_one_away("a", "a")  # should return True
+print(ans)
+ans = is_one_away("abcdef", "abqdef")  # should return True
+print(ans)
+ans = is_one_away("abcdef", "abccef")  # should return True
+print(ans)
+ans = is_one_away("abcdef", "abcde")  # should return True
+print(ans)
+ans = is_one_away("aaa", "abc")  # should return False
+print(ans)
+ans = is_one_away("abcde", "abc")  # should return False
+print(ans)
+ans = is_one_away("abc", "abcde")  # should return False
+print(ans)
+ans = is_one_away("abc", "bcc")  # should return False
+print(ans)
