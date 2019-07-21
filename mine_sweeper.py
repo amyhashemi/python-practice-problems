5df23b9a4cfaac944d837796d7799a02c9c22302
Amy Hashemi <amyhashemi@gmail.com>
2019-07-21 14:55:14 -0700

    added click method to minesweeper

--- mine_sweeper.py
+++ mine_sweeper.py
@@ -1,3 +1,4 @@
+import queue
 
 def mine_sweeper(bombs, num_rows, num_cols):
     '''
@@ -29,14 +30,106 @@ def click(field, num_rows, num_cols, given_i, given_j):
             num_cols: number of columns in the field
             given_i: row position where the user wants to click
             given_j: column position where the user wants to click
+
+            Soluton uses Breadth-First search to scan all surrounding cells for 0's
     '''
+    #to_check queue for storing the next coordinates to check - pop off the top as we check them
+    to_check = queue.Queue()
+    current_i = given_i
+    current_j = given_j
 
-    
+    if (field[current_i][current_j] != 0):
+        return field
+    else:
+        #store cell coordinates in to_check if it is a zero and then change the value to -2 -- we want to check the cells surrounding that cell later
+        field[current_i][current_j] = -2
+        to_check.put((current_i, current_j))
+
+        while not to_check.empty():
+            (current_i, current_j) = to_check.get()
+            for i in range(current_i-1, current_i+2):
+                for j in range(current_j-1, current_j+2):
+                     if (0 <= i < num_rows and 0 <= j < num_cols and field[i][j] == 0): #only if the current coordnates are actually positions in the array do we check
+                        to_check.put((i, j))
+                        field[i][j] = -2
+
+    return field
 
 
 
-field = mine_sweeper(([1,0],[0,1]), 3, 4)
+
+
+# using output from the last function
+# field = mine_sweeper(([1,0],[0,1]), 3, 4)
+
+# field = click(field, 3, 4, 0, 3)
+
+# print(field[0])
+# print(field[1])
+# print(field[2])
+
+#test cases
+
+field1 = [[0, 0, 0, 0, 0],
+          [0, 1, 1, 1, 0],
+          [0, 1, -1, 1, 0]]
+
+# click(field1, 3, 5, 2, 2) should return:
+# [[0, 0, 0, 0, 0],
+#  [0, 1, 1, 1, 0],
+#  [0, 1, -1, 1, 0]]
+
+field = click(field1, 3, 5, 2, 2)
 
 print(field[0])
 print(field[1])
 print(field[2])
+print("_________")
+
+# click(field1, 3, 5, 1, 4) should return:
+# [[-2, -2, -2, -2, -2],
+#  [-2, 1, 1, 1, -2],
+#  [-2, 1, -1, 1, -2]]
+
+field = click(field1, 3, 5, 1, 4)
+
+print(field[0])
+print(field[1])
+print(field[2])
+print("_________")
+
+# Next test case
+
+field2 = [[-1, 1, 0, 0],
+          [1, 1, 0, 0],
+          [0, 0, 1, 1],
+          [0, 0, 1, -1]]
+
+# click(field2, 4, 4, 0, 1) should return:
+# [[-1, 1, 0, 0],
+#  [1, 1, 0, 0],
+#  [0, 0, 1, 1],
+#  [0, 0, 1, -1]]
+
+field = click(field2, 4, 4, 0, 1)
+
+print(field[0])
+print(field[1])
+print(field[2])
+print("_________")
+
+# click(field2, 4, 4, 1, 3) should return:
+# [[-1, 1, -2, -2],
+#  [1, 1, -2, -2],
+#  [-2, -2, 1, 1],
+#  [-2, -2, 1, -1]]
+
+field = click(field2, 4, 4, 1, 3)
+
+print(field[0])
+print(field[1])
+print(field[2])
+print("_________")
+
+
+
