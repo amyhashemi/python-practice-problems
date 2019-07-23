e600d05d703fc6ba755ca02cb8b9592174668f66
Amy Hashemi <amyhashemi@gmail.com>
2019-07-22 23:22:25 -0700

    longest-palindrome not complete yet

--- longest-palindromic-string.py
+++ longest-palindromic-string.py
@@ -1,20 +1,35 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        p1 = 0
-        p2 = len(s) - 1
 
-        for i in range(len(s)):
-            for j in range(1, len(s)):
-            
-                p1 = s[i]
-                p2 = s[len(s)-j]
-                
-                print(p1)
-                print(p2)
+        i = 0
+        j = 0
+        length = len(s)
+        largest = 0
+        palindices = []
+        longestPalindrome = ''
+        checkarray = [[0 for item in range(0, length)] for num in range(0, length)]
+        
+        for num in range(0, length):
+            checkarray[num][num] = 1
 
-                if p1 == p2:
-                    p1 = s[i+1]
-                
+        
+        for i in range(0, length-1):
+            j+=1
+            if s[i] == s[j]:
+                checkarray[i][j] = checkarray[i+1][j-1]
+            else:
+                checkarray[i][j] = max(checkarray[i][j-1], checkarray[i+1][j])
+        
+        for i in checkarray:
+            for j in checkarray:
+                if checkarray[i][j] >= largest:
+                    palindices[0] = checkarray[i]
+                    palindices[1] = checkarray[j]
+
+        for item in range(palindices[0], palindices[1]):
+            longestPalindrome + s[item]
+
+        print(longestPalindrome)
 
         return
 
